bcbd7661fa0411274545fd41e9656d51dc75bcf7
superomosh94 <omondo94@gmail.com>
2025-09-29 18:56:06 +0300

    updated db.js

--- app/controllers/config/db.js
+++ app/controllers/config/db.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD ||'root',
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'edulms_project',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
