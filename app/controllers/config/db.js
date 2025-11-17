8cc813a21b6641f26d0aa738962fa8a480eda5e4
superomosh94 <omondo94@gmail.com>
2025-11-17 21:08:47 +0300

    fix: Update database connection configuration to remove default values and ensure SSL is enabled

--- app/controllers/config/db.js
+++ app/controllers/config/db.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
-const mysql = require('mysql2/promise');
+require('dotenv').config()
+const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'edulms_project',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
-});
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: true
+  }
+})
 
-module.exports = pool;
+module.exports = pool
